e2824ce862fb2810c694a0001e71b79deef5570e
ASURA-KaMi <fairee813@gmail.com>
2021-08-24 02:40:45 +0300

    Add travis ci

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -2,14 +2,6 @@ package com.tsystems.javaschool.tasks.calculator;
 
 public class Calculator {
 
-    /**
-     * Evaluate statement represented as string.
-     *
-     * @param statement mathematical statement containing digits, '.' (dot) as decimal mark,
-     *                  parentheses, operations signs '+', '-', '*', '/'<br>
-     *                  Example: <code>(1 + 38) * 4.5 - 1 / 2.</code>
-     * @return string value containing result of evaluation or null if statement is invalid
-     */
     private boolean isInt(String strnum){
         boolean answ = false;
         return answ;
