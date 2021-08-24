4c7e358951208ee371b0cd0ce351a1d46861dd23
ASURA-KaMi <fairee813@gmail.com>
2021-08-24 05:24:00 +0300

    Subsequence add solution

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,6 +1,8 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Subsequence {
 
@@ -14,7 +16,21 @@ public class Subsequence {
      */
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+        boolean isSequence = true;
+        if (x == null || y == null)
+            throw new IllegalArgumentException();
+        int searchPos = 0;
+        for (int i = 0; i < x.size(); i++){
+            if (!isSequence) break;
+            isSequence = false;
+            for (int j = searchPos; j < y.size(); j++){
+                if (x.get(i).equals(y.get(j))){
+                    isSequence = true;
+                    searchPos = j + 1;
+                    break;
+                }
+            }
+        }
+        return isSequence;
     }
 }
